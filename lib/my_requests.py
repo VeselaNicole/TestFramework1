3a38df5d5ba8b8a8054c3ba989f20025b1f09fad
VeselaNicole <56206131+VeselaNicole@users.noreply.github.com>
2023-04-19 17:43:00 +0300

    Logger

--- lib/my_requests.py
+++ lib/my_requests.py
@@ -1,5 +1,7 @@
 import requests
 
+from lib.logger import Logger
+
 
 class MyRequests():
     @staticmethod
@@ -28,6 +30,9 @@ class MyRequests():
         if cookies is None:
             cookies = {}
 
+
+        Logger.add_request(url, data, headers, cookies, method)
+
         if method == "GET":
             response = requests.get(url, params=data, headers=headers, cookies=cookies)
         elif method == "POST":
@@ -38,5 +43,7 @@ class MyRequests():
             response = requests.delete(url, data=data, headers=headers, cookies=cookies)
         else:
             raise Exception(f"Bad HTTP method '{method}'")
+
+        Logger.add_response(response)
 
         return response
